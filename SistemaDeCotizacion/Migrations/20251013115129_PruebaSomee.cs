f774ddeef92324370cf96d839d81bbcda29460d7
MaxYd821 <maxramirez0914@gmail.com>
2025-10-13 10:24:33 -0500

    Despliegue de pruebas en somme

---
+++ SistemaDeCotizacion/Migrations/20251013115129_PruebaSomee.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaDeCotizacion.Migrations
+{
+    /// <inheritdoc />
+    public partial class PruebaSomee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
